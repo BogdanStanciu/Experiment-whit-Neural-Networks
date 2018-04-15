a8f84c893ad3e28d65f97e8915755fdca0b45d54
Bogdan <bogdan@MacBook-Pro-di-Bogdan.local>
2018-04-15 18:02:27 +0200

    add comments

--- Perceptron/index.py
+++ Perceptron/index.py
@@ -2,13 +2,6 @@ import matplotlib.pyplot as plt
 import random
 from Perceptron import Perceptron
 
-def inizialize(n):
-
-    traing_set = [];
-    for i in range(n):
-        traing_set.append(Traing())
-
-
 
 if __name__ == "__main__":
     p = Perceptron(3, 0.0001)
@@ -16,6 +9,7 @@ if __name__ == "__main__":
 
     points = [];
 
+    # Create 1000 random x,y points 
     for i in range(0, 1000):
         x = random.randint(-100, 100);
         y = random.randint(-100, 100);
@@ -32,24 +26,28 @@ if __name__ == "__main__":
 
 
     # Print the point on graph
-
     for point in points:
         if point[-1] is 1:
             plt.plot(point[0], point[1], 'bo')
         else:
             plt.plot(point[0], point[1], 'yo')
 
-
+    # For each point i add the bias = 1 to the training input
+    # and after i train the perceptron
     for point in points:
         input = point[0:-1]
         label = point[-1]
         input.append(1)
         p.train(input, label)
 
+    # return the weights after train
     print(p.getWeights())
 
 
+    # Save the history of prediction, target
     history = []
+
+    # Make some prediciton
     for i in range(0,10):
         x = random.randint(-100, 100)
         y = random.randint(-100, 100)
@@ -63,11 +61,14 @@ if __name__ == "__main__":
         prediction = p.predict([x, y, 1])
         history.append([int(prediction), a])
 
+        # If the prediction is equal to the target
+        # i draw a green circle else a red circle
         if int(prediction) == a:
             plt.plot(x, y, 'go')
         else:
             plt.plot(x, y, 'ro')
 
     print(history)
+
     # show graph
     plt.show();
